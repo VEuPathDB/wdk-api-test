11b53c5a81cf8a7e6dc3dc2f35f41d0a5d137615
steve-fischer-200 <sfischer@upenn.edu>
2018-09-18 12:15:01 -0400

    add more DELETE tests

--- src/test/java/test/service/TempFileTest.java
+++ src/test/java/test/service/TempFileTest.java
@@ -16,24 +16,48 @@ public class TempFileTest extends TestBase {
   @Test
   @DisplayName("Create and delete temp file")
   void createAndDeleteTempFile() {
-      
-      Response response =
-	  RestAssured
-	  .given().multiPart("file", "this is the contents")
-	  .expect()
-	  .statusCode(HttpStatus.SC_NO_CONTENT)
-	  .when()
-	  .post(BASE_PATH);
-      
-      String id = response.getHeader("ID");
-      String cookieId = response.getCookie("JSESSIONID");
-      
-      RestAssured
-      .given()
-      .cookie("JSESSIONID", cookieId)
-      .expect()
-      .statusCode(HttpStatus.SC_NO_CONTENT)
-      .when().delete(DELETE_PATH, id);
+
+    // Test temp file create.  also grab cookie and ID
+    Response response = testTempFileCreate();
+    String cookieId = response.getCookie("JSESSIONID");
+    String tempFileId = response.getHeader("ID");
+
+    // test DELETE with irrelevant cookie. should get NOT FOUND
+    testTempFileDelete(tempFileId, getIrrelevantCookieId(), HttpStatus.SC_NOT_FOUND);
+
+    // test DELETE with silly ID. should get NOT FOUND
+    testTempFileDelete("silly-id", cookieId, HttpStatus.SC_NOT_FOUND);
+
+    // test real DELETE
+    testTempFileDelete(tempFileId, cookieId, HttpStatus.SC_NO_CONTENT);
+  }
+  
+  private Response testTempFileCreate() {
+    return
+    RestAssured
+    .given().multiPart("file", "this is the contents")
+    .expect()
+    .statusCode(HttpStatus.SC_NO_CONTENT)
+    .when()
+    .post(BASE_PATH);
+  }
+  
+  private void testTempFileDelete(String fileId, String cookieId, int expectedStatus) {
+    RestAssured
+    .given()
+    .cookie("JSESSIONID", cookieId)
+    .expect()
+    .statusCode(expectedStatus)
+    .when().delete(DELETE_PATH, fileId);    
+  }
+  
+  private String getIrrelevantCookieId() {
+    return RestAssured
+        .expect()
+        .statusCode(HttpStatus.SC_OK)
+        .when()
+        .get(SERVICE_PATH)
+        .getCookie("JSESSIONID");
   }
 
 }
