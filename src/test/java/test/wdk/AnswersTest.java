00ba3566408ab2524288c5c673d0b34c1d2acaeb
steve-fischer-200 <43149795+steve-fischer-200@users.noreply.github.com>
2018-10-19 15:13:02 -0400

    test we get the right number of records

--- src/test/java/test/wdk/AnswersTest.java
+++ src/test/java/test/wdk/AnswersTest.java
@@ -1,11 +1,15 @@
 package test.wdk;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static test.support.Conf.SERVICE_PATH;
+
+import java.util.List;
 
 import org.apache.http.HttpStatus;
 import org.gusdb.wdk.model.api.AnswerSpec;
 import org.gusdb.wdk.model.api.DefaultAnswerRequestBody;
 import org.gusdb.wdk.model.api.DefaultJsonAnswerFormatConfig;
+import org.gusdb.wdk.model.api.RecordInstance;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import test.support.Category;
 import test.support.util.AnswerUtil;
 import test.support.util.GuestRequestFactory;
@@ -35,8 +40,13 @@ public class AnswersTest extends TestBase {
     DefaultJsonAnswerFormatConfig formatConfig = AnswerUtil.getDefaultFormatConfigOneRecord();
     DefaultAnswerRequestBody requestBody = new DefaultAnswerRequestBody(answerSpec);
     requestBody.setFormatConfig(formatConfig);
-    _guestRequestFactory.jsonPayloadRequest(requestBody, HttpStatus.SC_OK,
+    Response response = _guestRequestFactory.jsonPayloadRequest(requestBody, HttpStatus.SC_OK,
         ContentType.JSON).when().post(BASE_PATH);
+    
+    // minimally, confirm we got exactly one record
+    List<RecordInstance> records = response.body().jsonPath().getList("records", RecordInstance.class);
+    assertEquals(records.size(), 1);
+
   }
 
 }
