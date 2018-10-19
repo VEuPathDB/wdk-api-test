817481c65c220f079afad35e818198ca3e95cad6
steve-fischer-200 <43149795+steve-fischer-200@users.noreply.github.com>
2018-10-19 13:54:33 -0400

    new proto RecordInstance class (empty for now)

---
+++ src/main/java/org/gusdb/wdk/model/api/RecordInstance.java
@@ -0,0 +1,11 @@
+package org.gusdb.wdk.model.api;
+
+/**
+ * The _start_ of a record instance model.
+ * TODO: add the many fields needed
+ * @author Steve
+ *
+ */
+public class RecordInstance {
+
+}
