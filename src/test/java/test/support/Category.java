a2ad4d13f6d524288754063f241e090d0e0157de
Elizabeth Harper <epharper@upenn.edu>
2018-10-24 13:35:12 -0400

    update category names

--- src/test/java/test/support/Category.java
+++ src/test/java/test/support/Category.java
@@ -6,21 +6,21 @@ public final class Category {
 
   public static final String MULTI_AUTH_TEST = "multi-auth";
 
-  public static final String AMOEBA_TEST        = "amoeba";
-  public static final String CLINEPI_TEST       = "clinepi";
-  public static final String CRYPTO_TEST        = "crypto";
-  public static final String EUPATH_TEST        = "eupath";
-  public static final String FUNGI_TEST         = "fungi";
-  public static final String GIARDIA_TEST       = "giardia";
-  public static final String MICROBIOME_TEST    = "microbiome";
-  public static final String MICROSPORIDIA_TEST = "microsporidia";
-  public static final String ORTHOMCL_TEST      = "orthomcl";
-  public static final String PIROPLASMA_TEST    = "piroplasma";
-  public static final String PLASMO_TEST        = "plasmo";
-  public static final String TOXO_TEST          = "toxo";
-  public static final String TRICH_TEST         = "trich";
-  public static final String TRITRYP_TEST       = "tritryp";
-  public static final String PUBLIC_STRATEGIES_TEST       = "public-strategies";
+  public static final String AMOEBA_TEST            = "amoebadb";
+  public static final String CLINEPI_TEST           = "clinepidb";
+  public static final String CRYPTO_TEST            = "cryptodb";
+  public static final String EUPATH_TEST            = "eupathdb";
+  public static final String FUNGI_TEST             = "fungidb";
+  public static final String GIARDIA_TEST           = "giardiadb";
+  public static final String MICROBIOME_TEST        = "microbiomedb";
+  public static final String MICROSPORIDIA_TEST     = "microsporidiadb";
+  public static final String ORTHOMCL_TEST          = "orthomcl";
+  public static final String PIROPLASMA_TEST        = "piroplasmadb";
+  public static final String PLASMO_TEST            = "plasmodb";
+  public static final String TOXO_TEST              = "toxodb";
+  public static final String TRICH_TEST             = "trichdb";
+  public static final String TRITRYP_TEST           = "tritrypdb";
+  public static final String PUBLIC_STRATEGIES_TEST = "public-strategies";
 
 
   private Category() {}
