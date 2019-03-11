61462f384c61e38fcd736cd1aa3455aab0879289
steve-fischer-200 <43149795+steve-fischer-200@users.noreply.github.com>
2019-03-11 19:55:14 -0400

    add new tests and correct for new API

--- src/test/java/test/wdk/RecordsTest.java
+++ src/test/java/test/wdk/RecordsTest.java
@@ -1,15 +1,20 @@
 package test.wdk;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import test.support.Category;
 import test.support.util.GuestRequestFactory;
 
 import static test.support.Conf.SERVICE_PATH;
 
 @DisplayName("Records")
 public class RecordsTest extends TestBase {
-  public static final String BASE_PATH = SERVICE_PATH + "/records";
+  public static final String BASE_PATH = SERVICE_PATH + "/record-types";
   public static final String EXP_PATH = BASE_PATH + "?format=expanded";
+  public static final String NAME_PATH = SERVICE_PATH + "/record-types/{recordClassName}";
+
 
   private final GuestRequestFactory req;
 
@@ -19,13 +24,22 @@ public class RecordsTest extends TestBase {
 
   @Test
   @DisplayName("GET " + BASE_PATH)
-  void getRecordNames() {
+  void getAllRecordNames() {
     req.jsonSuccessRequest().when().get(BASE_PATH);
   }
 
   @Test
   @DisplayName("GET " + EXP_PATH)
-  void getRecordDetails() {
+  void getAllRecordDetails() {
     req.jsonSuccessRequest().when().get(EXP_PATH);
   }
+  
+  @Test
+  @DisplayName("GET popsetSetquence record type")
+  @Tag (Category.PLASMO_TEST)
+  void getPopsetRecordDetails() {
+    req.jsonSuccessRequest().when().get(NAME_PATH, "popsetSequence");
+  }
+
+
 }
