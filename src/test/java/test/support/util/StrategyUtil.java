d457ea565817981617471517b1ad2fdf5cd42fed
Ryan Doherty <rdoherty@upenn.edu>
2019-04-05 02:36:51 -0400

    fix path

--- src/test/java/test/support/util/StrategyUtil.java
+++ src/test/java/test/support/util/StrategyUtil.java
@@ -20,7 +20,7 @@ public class StrategyUtil {
 
   public static final String BASE_PATH = UserUtil.BY_ID_PATH + "/strategies";
   public static final String BY_ID_PATH = BASE_PATH + "/{stratId}";
-  public static final String BY_ID_PATH_WITH_STEP_TREE = BY_ID_PATH + "/stepTree";
+  public static final String BY_ID_PATH_WITH_STEP_TREE = BY_ID_PATH + "/step-tree";
 
   public static StrategyUtil getInstance() {
     if (instance == null) {
