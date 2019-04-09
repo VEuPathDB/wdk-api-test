ecac2b3c1975b4f4ebf4be4735838c3a0cd6013a
Ryan Doherty <rdoherty@upenn.edu>
2019-04-09 19:16:24 -0400

    Add step tree wrapper object

---
+++ src/main/java/org/gusdb/wdk/model/api/StepTreeWrapper.java
@@ -0,0 +1,15 @@
+package org.gusdb.wdk.model.api;
+
+public class StepTreeWrapper {
+
+  private StepTreeNode _stepTree;
+
+  public void setStepTree(StepTreeNode stepTree) {
+    _stepTree = stepTree;
+  }
+
+  public StepTreeNode getStepTree() {
+    return _stepTree;
+  }
+
+}
